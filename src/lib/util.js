31e0f5b823b7a09f809480fa404b34234668e38b
zenoven <zenoven@gmail.com>
2020-07-14 10:22:08 +0800

    feat: promisify chrome APIs with bluebird

---
+++ src/lib/util.js
@@ -0,0 +1,27 @@
+import Promise, { reject } from 'bluebird';
+
+export const c = Object.assign({}, chrome);
+
+const promisifiedChromeAPIs = ['windows', 'tabs', 'browserAction'];
+const promisifier = (method) => {
+  return function (...args) {
+    let self = this;
+    return new Promise((resolve, reject) => {
+      if (c.runtime.lastError) {
+        return reject(c.runtime.lastError)
+      }
+      args.push(resolve);
+      method.call(self, ...args);
+    })
+  }
+};
+
+promisifiedChromeAPIs.forEach((apiName) => {
+  c[apiName] = Promise.promisifyAll(c[apiName], { promisifier});
+});
+
+export const getAllTabs = () => {
+  return c.windows.getCurrentAsync().then((win) => {
+    return c.tabs.queryAsync({ windowId: win.id });
+  });
+};
--- src/options.js
+++ src/options.js
@@ -1,1 +1,3 @@
-console.log('h')
+import { getAllTabs, windows, c } from 'lib/util';
+
+getAllTabs().then(tabs => console.log('tabs:', tabs))
