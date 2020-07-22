84264aa4d323f78727b5d36b98b81da2f31b5038
zenoven <zenoven@gmail.com>
2020-07-22 09:58:53 +0800

    fix: redux state changed after views hot reload

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { init } from '@rematch/core';
+import immerPlugin from '@rematch/immer';
+import * as models from 'models';
+import { Provider } from 'react-redux';
 import APP from 'views/app';
+
+const immer = immerPlugin();
+const store = init({
+  models,
+  plugins: [immer],
+});
+
 export const render = () => {
-  ReactDOM.render(<APP />, document.getElementById('app'))
+  ReactDOM.render(
+    (
+      <Provider store={store}>
+        <APP />
+      </Provider>
+    ),
+    document.getElementById('app')
+  );
 }
 
 export default { render };
--- src/views/app.jsx
+++ src/views/app.jsx
@@ -1,22 +1,11 @@
 import { hot } from 'react-hot-loader/root';
 import React from 'react';
-import { init } from '@rematch/core';
-import immerPlugin from '@rematch/immer';
-import * as models from '../models';
-import { Provider } from 'react-redux';
 import Options from 'views/options';
-const immer = immerPlugin();
-const store = init({
-  models,
-  plugins: [immer],
-});
 
 
 const APP = () => {
   return (
-    <Provider store={store}>
-      <div><Options></Options></div>
-    </Provider>
+    <Options></Options>
   )
 };
 
